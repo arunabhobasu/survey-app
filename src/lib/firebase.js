d11accdcc40cbc22b7b1a5f97584a55abc075ffa
Arunabho Basu <54653969+arunabhobasu@users.noreply.github.com>
2026-04-19 11:30:52 -0400

    deployment bug fix

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp, getApps } from "firebase/app";
-import { initializeFirestore, persistentLocalCache, persistentMultipleTabManager } from "firebase/firestore";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
